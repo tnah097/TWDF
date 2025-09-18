a27b783752e1cc6bc57a1fa570656e92ec1e4732
TWDF <thanaporn.map@thaimooc.ac.th>
2025-09-19 02:55:45 +0700

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const { Pool } = require("pg");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; 
 
 // ใช้ connectionString จาก .env
 const pool = new Pool({
